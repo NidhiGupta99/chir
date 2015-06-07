5982607a9f99c86c2a45cf60451e2dd5cb0823e7
NidhiGupta99 <luckbynid@gmail.com>
2015-06-07 15:24:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,10 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(session({
-  secret: 'keyboard cat'
+  secret: 'keyboard cat',
+    proxy: true,
+    resave: true,
+    saveUninitialized: true
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
