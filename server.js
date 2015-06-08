c47cd4c50a13dd755b10deddf30b36ed39d3508f
NidhiGupta99 <luckbynid@gmail.com>
2015-06-08 13:31:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,15 +41,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(session({
-	 cookie: { maxAge: 1000*60*2 } ,
-    secret: 'keyboard cat',
-     
-  
-    proxy: true,
-   resave: true,
-    saveUninitialized: true
-}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
