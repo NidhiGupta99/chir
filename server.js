5c63d3ddcd38c117a34fe146a915d811855bfde4
NidhiGupta99 <luckbynid@gmail.com>
2015-06-08 12:01:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ var mongoose = require('mongoose');
 
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + 'chirp';
-   mongoose.Connect(mongodb_connection_string);
+   mongoose.connect(mongodb_connection_string);
 }else{mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + 'chirp';}
 
 
