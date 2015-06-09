30d2ba13012b37d8f613d1641ea0e267c89cf09b
NidhiGupta99 <luckbynid@gmail.com>
2015-06-09 21:18:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var http = require('http');
 
 var path = require('path');
 var favicon = require('serve-favicon');
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 
 //var nodeEnv = process.env.NODE_ENV || 'unknown';
