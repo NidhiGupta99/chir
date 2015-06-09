fe5704e47648c98f0d5efe4271aba4d0e8423341
NidhiGupta99 <luckbynid@gmail.com>
2015-06-09 12:05:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ var express = require('express');
 
 var path = require('path');
 var favicon = require('serve-favicon');
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
-//var nodeEnv = process.env.NODE_ENV || 'unknown';
+
+var nodeEnv = process.env.NODE_ENV || 'unknown';
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
